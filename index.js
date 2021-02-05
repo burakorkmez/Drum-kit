ab29657731ad71c5d230bd35c328b99cca61fd8b
burakorkmez <75279146+burakorkmez@users.noreply.github.com>
2021-02-05 19:28:46 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+window.addEventListener("keydown", playSound);
+
+function playSound(e){
+  const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key = "${e.keyCode}"]`);
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');
+}
+
+function removeTransition(e){
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
